ac600267446ef87323039697e84b3fa17fae3360
lww <1344382941@qq.com>
2025-01-21 17:00:14 +0800

    note改为json传输

--- src/network/base.js
+++ src/network/base.js
@@ -1,76 +1,71 @@
-import { request,LoginRequest } from "./request";
+import { request, LoginRequest } from "./request";
 
-export function getFileList(filePath, queryWord) {
-    return request({
-        method: "post",
-        url: "/file/list1",
-      params: {         
-        path:filePath,
-        keyword:queryWord,
-      }
-    })}
+export function listNote(pageNum, pageSize) {
+  return request({
+    method: "post",
+    url: "/note/listNote",
+    params: {
+      pageNum: pageNum,
+      pageSize: pageSize,
+    }
+  })
+}
 
-    export function listNote(pageNum, pageSize) {
-      return request({
-          method: "post",
-          url: "/note/listNote",
-        params: {         
-          pageNum:pageNum,
-          pageSize:pageSize,
-        }
-      })}
+export function getNote(id) {
+  return request({
+    method: "get",
+    url: "/note/getNote",
+    params: {
+      id: id,
+    }
+  })
+}
 
-      export function getNote(id) {
-        return request({
-            method: "post",
-            url: "/note/getNote",
-          params: {         
-            id:id,
-          }
-        })}
-
-        export function editNote(id,content,title) {
-          return request({
-              method: "post",
-              url: "/note/editNote",
-            params: {         
-              id:id,
-              content:content,
-              title:title
-            }
-          })}
+export function editNote(id, content, title) {
+  return request({
+    method: "post",
+    url: "/note/editNote",
+    data: {
+      id: id,
+      content: content,
+      title: title
+    }
+  })
+}
 
 
-          export function addNote(id,content,title) {
-            return request({
-                method: "post",
-                url: "/note/addNote",
-              params: {         
-                id:id,
-                content:content,
-                title:title
-              }
-            })}
+export function addNote(id, content, title) {
+  return request({
+    method: "post",
+    url: "/note/addNote",
+    data: {
+      id: id,
+      content: content,
+      title: title
+    }
+  })
+}
 
 
-            export function deleteNoteItem(id) {
-              return request({
-                  method: "post",
-                  url: "/note/deleteNote",
-                params: {         
-                  id:id,
-                }
-              })}
+export function deleteNoteItem(id) {
+  return request({
+    method: "get",
+    url: "/note/deleteNote",
+    params: {
+      id: id,
+    }
+  })
+}
 
 
-              
-            export  function login(username,password) {
-              return LoginRequest({
-                url: '/user/login',
-                method:'post',
-                params: {     
-                  username,
-                  password
-                }
-              })
-            }
+
+export function login(username, password) {
+  return LoginRequest({
+    url: '/user/login',
+    method: 'post',
+    params: {
+      username,
+      password
+    }
+  })
+}
