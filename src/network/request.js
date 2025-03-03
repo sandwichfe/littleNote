669aa7169fe4cf32a863873ce4d5e3342aac169e
lww <1344382941@qq.com>
2025-03-03 23:03:53 +0800

    resp

--- src/network/request.js
+++ src/network/request.js
@@ -3,8 +3,8 @@ import { ElMessage} from 'element-plus';
 import Cookies from 'js-cookie'
 import router from '../router';
 
-// const requestIp = "127.0.0.1";
-const requestIp = "49.235.149.110";
+const requestIp = "127.0.0.1";
+// const requestIp = "49.235.149.110";
 // 环境
 
 export function request(config) {
@@ -21,8 +21,6 @@ export function request(config) {
         // let accessToken = store.getters.currentToken;
         let accessToken = Cookies.get("loginToken")
         if(accessToken){
-            // window.location.href="/login"
-            // Message("登录已失效请重新登录！");
             config.headers["Authorization"] = accessToken;
         }
          return config;
@@ -44,37 +42,6 @@ export function request(config) {
         return Promise.reject(err);
      });  
 
-// 响应式拦截
-instance.interceptors.response.use(
-    // res => res.data,
-    res => res,
-    err => handleError(err)
-);
-
-// 统一处理错误
-function handleError(err) {
-    console.error('Error details:', err);  // 打印错误详情
-    if (err.response) {
-        // 客户端收到服务器的错误响应
-        const { status, data } = err.response;
-        if (status === 401) {
-            ElMessage.error(data?.msg || "登录失效，请重新登录！");
-            router.push("/login");
-        } else {
-            ElMessage.error("发生了一点小错误，正在紧急修复中~");
-        }
-
-        return Promise.reject(err); // 返回拒绝的Promise以阻止进一步的处理
-    } else if (err.request) {
-        // 请求已发出但没有收到响应
-        ElMessage.error("请求发送失败，请检查网络连接。");
-        return Promise.reject(err);
-    } else {
-        // 其他错误
-        ElMessage.error("发生了未知错误，请稍后再试。");
-        return Promise.reject(err);
-    }
-}
     return instance(config);
 }
 
@@ -91,11 +58,8 @@ export function LoginRequest(config) {
     instance.interceptors.request.use(config => {
         // console.log(config);
         //请求头
-        // let accessToken = store.getters.currentToken;
         let accessToken = Cookies.get("loginToken")
         if(accessToken){
-            // window.location.href="/login"
-            // Message("登录已失效请重新登录！");
             config.headers["Authorization"] = accessToken;
         }
          return config;
@@ -104,12 +68,21 @@ export function LoginRequest(config) {
      });
  
  
-// 响应式拦截
-instance.interceptors.response.use(
-    res => res.data,
-    err => handleError(err)
-);
-
+     // 响应式拦截
+     instance.interceptors.response.use(res => {
+        if(401==res.data.code) {
+           ElMessage.success(res.data.msg);
+           router.push("/login",()=>{})
+        }
+        return res.data;
+    }, err => {
+        if(401==err.response.data.code) {
+            ElMessage.success(err.response.data.msg);
+            router.push("/login",()=>{})
+         }
+       console.log(err);
+       return Promise.reject(err);
+    });  
 
 
     return instance(config);
