ed615ba3bda441fd569fc29c9ea778d7bce1c3be
lww <1344382941@qq.com>
2025-04-05 01:19:19 +0800

    菜单

--- src/router/index.js
+++ src/router/index.js
@@ -11,10 +11,13 @@ const constantRoutes = [
   },
   {
     path: '/',
+    name: 'Layout',  // 添加名称
     component: ManageLayout,
-    children: [] // 动态添加的子路由
+    redirect: '/user',
+    children: []
   }
 ]
+
 
 // 动态路由映射表
 const componentMap = {
@@ -31,7 +34,37 @@ const router = createRouter({
   routes: constantRoutes
 })
 
-// 添加路由守卫
+export function generateRoutes(menuData) {
+  const routes = []
+  
+  function generateRoute(menu) {
+    return {
+      path: menu.path.replace('/', ''), // 移除开头的斜杠
+      name: menu.name,
+      component: componentMap[menu.path],
+      meta: {
+        title: menu.title,
+        icon: menu.icon,
+        permission: menu.permission
+      }
+    }
+  }
+
+  // 修改路由生成逻辑
+  menuData.forEach(menu => {
+    if (menu.children && menu.children.length > 0) {
+      menu.children.forEach(child => {
+        routes.push(generateRoute(child))
+      })
+    } else if (menu.type === 1) { // 只添加类型为1的菜单
+      routes.push(generateRoute(menu))
+    }
+  })
+
+  return routes
+}
+
+// 修改路由守卫
 router.beforeEach(async (to, from, next) => {
   const token = Cookies.get('loginToken')
   
@@ -50,8 +83,9 @@ router.beforeEach(async (to, from, next) => {
       const response = await getCurrentUserMenus()
       if (response.code === 200) {
         const routes = generateRoutes(response.data)
+        // 将所有路由添加为根路由的子路由
         routes.forEach(route => {
-          router.addRoute(route)
+          router.addRoute('Layout', route)
         })
         next({ ...to, replace: true })
       } else {
@@ -67,42 +101,6 @@ router.beforeEach(async (to, from, next) => {
     next()
   }
 })
-
-export function generateRoutes(menuData) {
-  const routes = []
-  
-  function generateRoute(menu) {
-    const route = {
-      path: menu.path,
-      name: menu.name,
-      component: componentMap[menu.path],
-      meta: {
-        title: menu.title,
-        icon: menu.icon,
-        permission: menu.permission
-      }
-    }
-
-    if (menu.children && menu.children.length > 0) {
-      route.children = menu.children.map(child => generateRoute(child))
-    }
-
-    return route
-  }
-
-  // 将生成的路由添加到根路由的 children 中
-  menuData.forEach(menu => {
-    if (menu.children && menu.children.length > 0) {
-      menu.children.forEach(child => {
-        routes.push(generateRoute(child))
-      })
-    } else {
-      routes.push(generateRoute(menu))
-    }
-  })
-
-  return routes
-}
 
 export default router
 
